cf4e4bbb76ca6d34f1b1b712b313babca0cd46e4
Lucas Frazao <lucasnfrazao@icloud.com>
2025-05-14 22:26:01 -0300

    new url

--- vestibulario-react/src/config.js
+++ vestibulario-react/src/config.js
@@ -2,4 +2,4 @@ const DEV = window.location.hostname === "localhost";
 
 export const API_BASE = DEV
   ? "http://localhost:4000"
-  : "http://3.80.100.133:4000";
+  : "https://projetotccnode.onrender.com";
